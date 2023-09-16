2a0d4b301ed2861ffc432d2436d33b8d58ff7ad6
Watermellon86 <91654341+MiroslavDina@users.noreply.github.com>
2023-09-16 23:27:14 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'watermellon86'
+description 'A simple script /do command for qbcore'
+version '1.0.0'
+
+client_script 'client.lua'
+
+dependency 'qb-core'
